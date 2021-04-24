e40fe84a48489a1252da4ac1dd86665d96440c20
Kevin Stevens <kcstevens@mac.com>
2021-04-24 17:41:38 -0400

    Addition of all market types to basket.

--- HenrysLib/Basket.cs
+++ HenrysLib/Basket.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace HenrysLib
 {
     public class Basket
     {
+        private int _soup;
+        private int _bread;
+        private int _milk;
+        private int _apples;
+
+        public int Soup   => _soup;
+        public int Bread  => _bread;
+        public int Milk   => _milk;
+        public int Apples => _apples;
+        
+        public void AddSoup()
+        {
+            _soup = 1;
+        }
+        public void AddBread()
+        {
+            _bread = 1;
+        }
+
+        public void AddMilk()
+        {
+            _milk = 1;
+        }
+
+        public void AddApples()
+        {
+            _apples = 1;
+        }
     }
 }
--- HenrysTest/BasketTests.cs
+++ HenrysTest/BasketTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.ComTypes;
 using HenrysLib;
 using NUnit.Framework;
 
@@ -5,16 +6,45 @@ namespace HenrysTests
 {
     public class Tests
     {
+        private Basket basket;
         [SetUp]
         public void Setup()
         {
+            basket = new Basket();
         }
 
         [Test]
         public void CanCreateABasket()
         {
-            var basket = new Basket();
             Assert.NotNull(basket);
+        }
+
+        [Test]
+        public void CanAddATinOfSoupToABasket()
+        {
+            basket.AddSoup();
+            Assert.AreEqual(1,basket.Soup);
+        }
+
+        [Test]
+        public void CanAddALoafOfBreadToABasket()
+        {
+            basket.AddBread();
+            Assert.AreEqual(1,basket.Bread);
+        }
+
+        [Test]
+        public void CanAddMilkToABasket()
+        {
+            basket.AddMilk();
+            Assert.AreEqual(1,basket.Milk);
+        }
+
+        [Test]
+        public void CanAddApplesToABasket()
+        {
+            basket.AddApples();
+            Assert.AreEqual(1,basket.Apples);
         }
     }
 }
